75bb3c4f55a1ace8e9e9817e195e82ea60fa4286
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-18 18:26:40 -0500

    oops.. enable

--- js/DA3_CatFighter.js
+++ js/DA3_CatFighter.js
@@ -3,6 +3,7 @@ function Catfighter(game)
 	this.game = game;
 	this.sprite = this.game.add.sprite(15, 15, 'catsheet', 0);
 	this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+	this.sprite.body.allowGravity = true;
 	this.sprite.animations.add('idle', [0, 1, 2, 3], 20, true);
 	this.sprite.animations.add('walk', [16, 17, 18, 19, 20, 21, 22, 23], 20, true);
 	this.sprite.animations.play('idle');
