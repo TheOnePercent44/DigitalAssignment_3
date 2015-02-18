cb0b9a9b88cca0287d7512dc695262d6a60d9699
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-18 18:33:10 -0500

    wooooo

--- js/DA3_Game.js
+++ js/DA3_Game.js
@@ -30,7 +30,7 @@ GrudgeMatch.Game.prototype = {
 
         //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
 		map = this.game.add.tilemap('map');
-		map.addTilesetImage('greenBlock', 'greenBlock_32x32');
+		map.addTilesetImage('greenBlock_32x32', 'greenBlock_32x32');
 		var layer = map.createLayer('Tile Layer 1');
 		var player = CatFighter(this.game);
     },
