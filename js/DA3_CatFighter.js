018ebadc24023182e8c5cf5b29b3da074f45ffce
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-18 21:54:24 -0500

    context correction

--- js/DA3_CatFighter.js
+++ js/DA3_CatFighter.js
@@ -45,6 +45,6 @@ function Catfighter(game, xcoord, ycoord)
 	
 	this.land = function(player, layer)//accepts two arguments for compatibility with collide
 	{
-		player.sprite.animations.play('landing');
+		this.sprite.animations.play('landing');
 	}
 }
