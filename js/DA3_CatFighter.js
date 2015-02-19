d7cfc61369585dba21d66860f815aeb14b0c5042
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-18 21:12:23 -0500

    dwads

--- js/DA3_CatFighter.js
+++ js/DA3_CatFighter.js
@@ -5,14 +5,15 @@ function Catfighter(game, xcoord, ycoord)
 	this.sprite.anchor.setTo(0.5, 0.5);
 	this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
 	this.sprite.body.allowGravity = true;
-	this.sprite.animations.add('idle', [0, 1, 2, 3], 10, true);
-	this.sprite.animations.add('walk', [16, 17, 18, 19, 20, 21, 22, 23], 10, true);
+	this.sprite.animations.add('idle', [0, 1, 2, 3], 15, true);
+	this.sprite.animations.add('walk', [16, 17, 18, 19, 20, 21, 22, 23], 15, true);
 	this.sprite.animations.play('idle');
 	
 	this.idle = function()
 	{
 		this.sprite.body.velocity.x = 0;
-		this.sprite.animations.stop();
+		if(this.sprite.animations.currentAnim != 'idle')
+			this.sprite.animations.stop(null, true);
 		this.sprite.animations.play('idle');
 	}
 	
@@ -20,7 +21,7 @@ function Catfighter(game, xcoord, ycoord)
 	{
 		this.sprite.animations.stop();
 		this.sprite.body.velocity.x = 100;
-		this.sprite.animations.play('walk');
+		this.sprite.animations.stop(null, true);
 		this.sprite.scale.x = 1;
 	}
 	
@@ -28,7 +29,7 @@ function Catfighter(game, xcoord, ycoord)
 	{
 		this.sprite.animations.stop();
 		this.sprite.body.velocity.x = -100;
-		this.sprite.animations.play('walk');
+		this.sprite.animations.stop(null, true);
 		this.sprite.scale.x = -1;
 	}
 	//return this;
