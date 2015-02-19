c9ae2c6c650fa4d6678f4004d6761e89f752ec34
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-18 20:46:28 -0500

    enable walking?

--- js/DA3_CatFighter.js
+++ js/DA3_CatFighter.js
@@ -5,8 +5,23 @@ function Catfighter(game, xcoord, ycoord)
 	this.sprite.anchor.setTo(0.5, 0.5);
 	this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
 	this.sprite.body.allowGravity = true;
-	this.sprite.animations.add('idle', [0, 1, 2, 3], 20, true);
-	this.sprite.animations.add('walk', [16, 17, 18, 19, 20, 21, 22, 23], 20, true);
+	this.sprite.animations.add('idle', [0, 1, 2, 3], 10, true);
+	this.sprite.animations.add('walk', [16, 17, 18, 19, 20, 21, 22, 23], 10, true);
 	this.sprite.animations.play('idle');
+	
+	this.idle = function()
+	{
+		this.sprite.animations.stop();
+		this.sprite.animations.play('idle');
+	}
+	
+	this.runRight = function(xspeed)
+	{
+		this.sprite.body.velocity.x = xspeed;
+		if(xspeed < 0)
+			this.sprite.scale(-1, 1);
+		else
+			this.sprite.scale(1, 1);
+	}
 	//return this;
 }
--- js/DA3_Game.js
+++ js/DA3_Game.js
@@ -51,6 +51,15 @@ GrudgeMatch.Game.prototype = {
 
         //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
 		this.game.physics.arcade.collide(player.sprite, layer);
+		
+		if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)
+		{
+			player.runRight(30);
+		}
+		else
+		{
+			player.idle();
+		}
     },
 
     quitGame: function (pointer) {
